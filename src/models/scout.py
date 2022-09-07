a485cd52c3a1922238d4da5c17c0643c1306ec74
AyushStarGupta <37546890+AyushCodez@users.noreply.github.com>
2022-09-08 01:32:36 +0530

    commit

--- src/models/scout.py
+++ src/models/scout.py
@@ -1,34 +1,39 @@
+import random
+
 class Scout():
     
-    def __init__(self,home_coords):
-        self.home_coords = home_coords
+    def __init__(self):
         self.active = []
-        self.loc = []
         self.num = 0
+        self.timetofind = 60*(5) # frames
+        self.death_time = []
+        self.time_taken = []
 
     def create(self,number = 1):
         for i in range(number):
             self.active.append(0)
-            self.loc.append(self.home_coords) #Home coords
+            self.death_time.append(random.randint(self.timetofind//2,int(self.timetofind*1.5)))
+            self.time_taken.append(0)
         self.num+=number
 
-    def send(self, index, coords: tuple):
+    def send(self, index):
         #TODO: Animate scout moving from one place to another
         if not(self.active[index]):
-            self.loc[index] = coords[:]
             self.active[index] = 1
 
-    def reveal(self,index):
-        def remove_clouds(x,y):
+    def reveal(self,index,loc):
+        def remove_clouds(loc):
             # TODO: reveal location
             pass
-        remove_clouds(self.loc[index][0],self.loc[index][1])
-        self.loc[index] = self.home_coords # Home coords
+        remove_clouds(loc)
         self.active[index] = 0
+        self.death_time[index] = (random.randint(self.timetofind//2,int(self.timetofind*1.5)))
+        self.time_taken[index] = 0
 
     def die(self,index):
         self.num-=1
-        self.loc.pop(index)
-        self.active.pop(index)
+        self.active[index] = 'X'
+        self.death_time[index] = 'X'
+        self.time_taken[index] = 'X'
 
 
--- src/screens/map_screen.py
+++ src/screens/map_screen.py
@@ -11,66 +11,123 @@ def run_map_screen(game):
     # game loop
     base_coords = (W/2,H/2)
     # Draw screen
-    Scouts = Scout(base_coords)
+    Scouts = Scout()
     Scouts.create(2)
     surface = game.display_surface
+    modal_showing = False
+    x_btn = None
+
+    def show_modal(title, text, color):
+
+        nonlocal modal_showing
+        modal_showing = True
+        maj_sur = pygame.Surface((800, 600))
+        maj_sur.set_alpha(180)
+        maj_sur.fill((0,0,0))
+        surface.blit(maj_sur, (0, 0))
+        text_aaa = pygame.font.Font('freesansbold.ttf', 25).render(title, True, color)
+        text_bbb = pygame.font.Font('freesansbold.ttf', 25).render(text, True, (255, 255, 255))
+
+        surf = pygame.Surface((500,
+                               20 + text_aaa.get_height() + 5 + text_bbb.get_height() + 20))
+
+        surf.blit(text_aaa, (20, 20))
+        surf.blit(text_bbb, (20, 20 + text_aaa.get_rect().height + 5))
+        nonlocal x_btn
+        x_btn = buttons.TextButton(surface=surf, pos=(470, 0), width=30, height=30, fg_color=(255,255,255),
+                           bg_color=(240, 0, 0), font= pygame.font.Font('freesansbold.ttf', 30),
+                           text=f'X')
+
+        surface.blit(surf, (W / 2 - 250, H / 2 -
+                                        (20 + text_aaa.get_height() + 5 + text_bbb.get_height() + 20)))
+        btn_x = W / 2 - 250 + 470
+        btn_y = H / 2 - (20 + text_aaa.get_height() + 5 + text_bbb.get_height() + 20) + 0
+        return btn_x, btn_y
 
     while game.running:
-        surface.fill((255,0,255))
-        test = buttons.TextButton(surface, (400,60), 100, 50, (0,0,0), (255,255,255), pygame.font.SysFont("arial", 20), "go back make map")
-        scout,scout_size = load_png("scout.png")
-        base,base_size = load_png("base.png")
-        surface.blit(base,base_coords)
-        create_scout = buttons.TextButton(surface, (0,570), 90, 20, (0,0,0), (255,255,255), pygame.font.SysFont("arial", 15), "Create Scout")
-        send_scout = buttons.TextButton(surface, (100,570), 90, 20, (0,0,0), (255,255,255), pygame.font.SysFont("arial", 15), "Send Scout")
-        text = pygame.font.Font('freesansbold.ttf', 15).render(f'Number of Scouts: {Scouts.num}  ', True, (0,0,0), (0,0,255))
-        surface.blit(text, (0,0))
-        text = pygame.font.Font('freesansbold.ttf', 15).render(f'Metal left: {10}  ', True, (0,0,0), (0,0,255)) # TODO: Put variable in
-        surface.blit(text, (0,15))
-        
-
 
         mouse_down = False
-
         for event in pygame.event.get():
-            if event.type == pygame.QUIT:
-                game.running = False
-            elif event.type == pygame.MOUSEBUTTONDOWN:
-                mouse_down = True
+                if event.type == pygame.QUIT:
+                    game.running = False
+                elif event.type == pygame.MOUSEBUTTONDOWN:
+                    mouse_down = True
 
-        if create_scout.hovered:
-            create_scout.toggle_bg((0,100,0))
+        if not(modal_showing):
+            surface.fill((255,0,255))
+            test = buttons.TextButton(surface, (400,60), 100, 50, (0,0,0), (255,255,255), pygame.font.SysFont("arial", 20), "go back make map")
+            scout,scout_size = load_png("scout.png")
+            base,base_size = load_png("base.png")
+            surface.blit(base,base_coords)
+            create_scout = buttons.TextButton(surface, (0,570), 90, 20, (0,0,0), (255,255,255), pygame.font.SysFont("arial", 15), "Create Scout")
+            send_scout = buttons.TextButton(surface, (100,570), 90, 20, (0,0,0), (255,255,255), pygame.font.SysFont("arial", 15), "Send Scout")
+            text = pygame.font.Font('freesansbold.ttf', 15).render(f'Available Scouts: {Scouts.active.count(0)}/{Scouts.num}  ', True, (0,0,0), (0,0,255))
+            surface.blit(text, (0,0))
+            text = pygame.font.Font('freesansbold.ttf', 15).render(f'Metal left: {10}  ', True, (0,0,0), (0,0,255)) # TODO: Put variable in
+            surface.blit(text, (0,15))
+                      
 
-            if mouse_down:
-                create_scout.toggle_bg((255,255,255))
-                Scouts.create()
-                
-        else:
-            test.toggle_bg((255,255,255))
+            if create_scout.hovered:
+                create_scout.toggle_bg((0,100,0))
 
-        if send_scout.hovered:
-            send_scout.toggle_bg((0,100,0))
-
-            if mouse_down:
-                send_scout.toggle_bg((255,255,255))
-                Scouts.create()
-                
-        else:
-            test.toggle_bg((255,255,255))
-
-        
-        if test.hovered:
-            test.toggle_bg((0,100,0))
-
-            if mouse_down:
+                if mouse_down:
+                    create_scout.toggle_bg((255,255,255))
+                    print("HI")
+                    Scouts.create()
+                    #TODO: use money/metal
+                    
+            else:
                 test.toggle_bg((255,255,255))
-                game.screen = 0
-                return
-                
+
+            if send_scout.hovered:
+                send_scout.toggle_bg((0,100,0))
+
+                if mouse_down:
+                    send_scout.toggle_bg((255,255,255))
+                    if 0 in Scouts.active:
+                        ind = Scouts.active.index(0)
+                        Scouts.send(ind)
+                    else:
+                        REL_COORDS = show_modal(title='Error!', text=f"You not have enough coin!", color=(100, 0, 0))
+
+            else:
+                test.toggle_bg((255,255,255))
+
+            
+
+            for i in range(len(Scouts.active)):
+
+                if Scouts.active[i] == 1:
+                    if Scouts.death_time[i]>0 and Scouts.time_taken[i] < Scouts.timetofind:
+                        Scouts.death_time[i]-= 1
+                        Scouts.time_taken[i]+= 1
+                    elif Scouts.death_time[i] <=0:
+                        print("DIE")
+                        Scouts.die(i)
+                    elif Scouts.time_taken[i] >= Scouts.timetofind:
+                        print("FOUND")
+                        Scouts.reveal(i, (0,0))
+
+            
+            if test.hovered:
+                test.toggle_bg((0,100,0))
+
+                if mouse_down:
+                    test.toggle_bg((255,255,255))
+                    game.screen = 0
+                    return
+                    
+            else:
+                test.toggle_bg((255,255,255))
+        
         else:
-            test.toggle_bg((255,255,255))
+            row, col = pygame.mouse.get_pos()
+
+            if REL_COORDS[0] <= row <= REL_COORDS[0] + 30 and REL_COORDS[1] <= col <= REL_COORDS[1] + 30:
+                if mouse_down:
+                    modal_showing = False
 
         pygame.display.update()
+        pygame.time.Clock().tick(60)
         
-    pass
 
