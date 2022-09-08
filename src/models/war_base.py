e9ad66e3db85ffa8bab01672ca8460455a63d15d
Pranjal Rastogi <25995849+PjrCodes@users.noreply.github.com>
2022-09-08 18:21:46 +0530

    Update war_base.py

--- src/models/war_base.py
+++ src/models/war_base.py
@@ -1,6 +1,6 @@
 class WarBase():
 
-    def __init__(self,type, slevel):
+    def __init__(self,type,level):
         self.level = level
         self.health = 10000 + (level*504)
         self.type
